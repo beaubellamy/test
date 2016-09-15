c0f13106683bc69d397358fe089ad42a2be3b62e
Beau Bellamy <beau.bellamy@hotmail.com>
2016-09-15 17:20:22 +1000

    Attempting to read the file and allocate the fields into arrays.

--- wagonMovement/wagonMovement.cs
+++ wagonMovement/wagonMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -50,36 +51,54 @@ namespace wagonMovement
             string filename = path + file;
 
             /* Structure of the volume data:
-             * Wagon Class
-             * Wagon Number
-             * Train Number
-             * Train Date
-             * Comodity
-             * Origin Code
-             * Planned Destination Code
-             * Actual Destination Code
-             * Attachment Time
-             * Detachment Time
-             * Tare Weight
-             * Gross Weight
-             * Distance Travelled
-             * Wagon Sequence
-             * Record ID
-             * Wagon Movement Count (smae as ID)
+             * 0 - Wagon Class
+             * 1 - Wagon Number
+             * 2 - Train Number
+             * 3 - Train Date
+             * 4 - Comodity
+             * 5 - Origin Code
+             * 6 - Planned Destination Code
+             * 7 - Actual Destination Code
+             * 8 - Attachment Time
+             * 9 - Detachment Time
+             * 10 - Tare Weight
+             * 11 - Gross Weight
+             * 12 - Distance Travelled
+             * 13 - Wagon Sequence
+             * 14 - Record ID
+             * 15 - Wagon Movement Count (smae as ID)
              */
             List<wagonDetails> wagon = new List<wagonDetails>();
 
             /* Read the text file */
             string[] lines = System.IO.File.ReadAllLines(filename);
+            char[] delimiters = {','};
 
+            double tareWeight = 0;
+            double grossWeight = 0;
+            DateTime attachmentTime = new DateTime(2000,1,1);
+            DateTime dettachmentTime = new DateTime(2000, 1, 1);
 
             /* Populate the wagonDetails array */
-            foreach (string lines in line)
+            foreach (string line in lines)
             {
-                string wagonID = lines[0] + " " + lines[1];
+                string record = line;
+                string[] fields = line.Split(delimiters);
+
+                string wagonID = Regex.Unescape(fields[0]) + " " + Regex.Unescape(fields[1]);
+                string origin = Regex.Unescape(fields[5]);
+                string plannedDestination = Regex.Unescape(fields[6]);
+                string destination = Regex.Unescape(fields[7]);
+                DateTime.TryParse(fields[8], out attachmentTime);
+                DateTime.TryParse(fields[9], out dettachmentTime);
+                double.TryParse(fields[11], out tareWeight);
+                double.TryParse(fields[10], out grossWeight);
+                double netWeight = grossWeight - tareWeight;
+
+                Console.WriteLine(Regex.Unescape(fields[0]));
             }
 
-
+            
             /* Write back to excel file. */
 
 
