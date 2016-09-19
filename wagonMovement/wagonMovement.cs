fe5c41b9fd598db6b3a06b3b836b260024d6636f
beaubellamy <beau.bellamy@hotmail.com>
2016-09-19 16:46:55 +1000

    Completed code to read data from text file and write the results to excel file.

--- wagonMovement/wagonMovement.cs
+++ wagonMovement/wagonMovement.cs
@@ -6,13 +6,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-Microsoft.Office.Interop.Excel.Application oXL;
-Microsoft.Office.Interop.Excel._Workbook oWB;
-Microsoft.Office.Interop.Excel._Worksheet oSheet;
-Microsoft.Office.Interop.Excel.Range oRng;
-object misvalue = System.Reflection.Missing.Value;
-
-
 namespace wagonMovement
 {
     
@@ -32,15 +25,16 @@ namespace wagonMovement
 
         public class wagonDetails
         {
-            string wagonID;
-            string origin;
-            string plannedDestination;
-            string destination;
-            DateTime attachmentTime;
-            DateTime detachmentTime;
-            double netWeight;
-
-            // constructor for all relevant fields
+            
+            public string wagonID;
+            public string origin;
+            public string plannedDestination;
+            public string destination;
+            public DateTime attachmentTime;
+            public DateTime detachmentTime;
+            public double netWeight;
+            
+            /* Constructor for all relevant fields */
             public wagonDetails(wagonDetails wagon)
             {
                 this.wagonID = wagon.wagonID;
@@ -73,7 +67,8 @@ namespace wagonMovement
                 this.detachmentTime = detachmentTime;
                 this.netWeight = netWeight;
             }
-            
+
+           
         }
         
 
@@ -88,7 +83,7 @@ namespace wagonMovement
             string file = @"\2015-16 FY Freight volumes test.txt";
             string filename = path + file;
 
-            // This will be redundant when the file is obtained from the dialogu box.
+            // This will be redundant when the file is obtained from the dialogue box.
             if (!File.Exists(filename))
             {
                 Console.WriteLine(filename+" does not exist. Check spelling and location.");
@@ -114,17 +109,29 @@ namespace wagonMovement
              * 15 - Wagon Movement Count (smae as ID)
              */
             List<wagonDetails> wagon = new List<wagonDetails>();
-            //wagonDetails wagonData = new wagonDetails();
-
             
-            Microsoft.Office.Interop.Excel.Application oXL;
-            //Microsoft.Office.Interop.Excel._Workbook oWB;
-            //Microsoft.Office.Interop.Excel._Worksheet oSheet;
-            //Microsoft.Office.Interop.Excel.Range oRng;
-            //object misvalue = System.Reflection.Missing.Value;
+            // Create a wagon object list from the wagon movement text file.
+            wagon = readWagonDataFile(filename);
 
 
-            /* Read the text file */
+            /*
+             * wagon movement algorithms ...
+             */
+
+
+            // Write the wagon details to excel.
+            writeToExcel(wagon);
+            
+
+        }
+        
+        /*
+         * Read the wagon movement data from the text file.
+         */
+        public static List<wagonDetails> readWagonDataFile(string filename)
+        {
+        
+            /* Read the all lines of the text file. */
             string[] lines = System.IO.File.ReadAllLines(filename);
             char[] delimiters = {','};
 
@@ -133,16 +140,12 @@ namespace wagonMovement
             DateTime attachmentTime = new DateTime(2000,1,1);
             DateTime detachmentTime = new DateTime(2000, 1, 1);
 
-            /* Populate the wagonDetails array */
+            /* Create the list of wagon objects. */
+            List<wagonDetails> wagon = new List<wagonDetails>();
+
+            /* Extract the wagon details from the data file. */
             foreach (string line in lines)
             {
-                
-                //string record = line;
-                string[] fields = line.Split(delimiters);
-                char [] newDelimeters = {'\'','"'};
-
-                string[] field0 = Regex.Unescape(fields[0]).Split(newDelimeters);
-                string[] field1 = Regex.Unescape(fields[1]).Split(newDelimeters);
                 
                 string wagonID = "";
                 string origin = "";
@@ -150,49 +153,144 @@ namespace wagonMovement
                 string destination = "";
                 double netWeight = 0;
 
+                /* Split the lines into the fields */
+                string[] fields = line.Split(delimiters);
+                char [] newDelimeters = {'\'','"'};
+
+                /* Check the number of fields */
+                if (fields.Count() != 16)
+                {
+                    Console.WriteLine("Incorrect number of fields detected in text file.");
+                    Console.WriteLine("Press enter to close...");
+                    Console.ReadLine();
+                    // Return empty wagon list.
+                    return wagon;       
+                }
+
+                /* Clean the wagon ID fields. */
+                string[] field0 = Regex.Unescape(fields[0]).Split(newDelimeters);
+                string[] field1 = Regex.Unescape(fields[1]).Split(newDelimeters);
+                
+                /*
+                 * Extract the cleaned fields and populate the wagon detail arrays.
+                 */
+                /* Wagon ID. */
                 if (field0.Count() == 3 && field1.Count() == 1)
                     wagonID = field0[1] + "-" + field1[0];
                 else
                     Console.WriteLine("Wagon ID configuration has not been accounted for.");
 
+                /* Wagon Origin. */
                 string[] field = Regex.Unescape(fields[5]).Split(newDelimeters);
-
                 if (field.Count() == 3)
                     origin = field[1];
                 else
                     Console.WriteLine("Origin configuration has not been accounted for.");
 
+                /* Wagon planned destination. */
                 field = Regex.Unescape(fields[6]).Split(newDelimeters);
-
                 if (field.Count() == 3)
                     plannedDestination = field[1];
                 else
                     Console.WriteLine("plannedDestination configuration has not been accounted for.");
 
+                /* Wagon destination. */
                 field = Regex.Unescape(fields[7]).Split(newDelimeters);
-
                 if (field.Count() == 3)
                     destination = field[1];
                 else
                     Console.WriteLine("destination configuration has not been accounted for.");
 
+                /* Remaining Wagon details. */
                 DateTime.TryParse(fields[8], out attachmentTime);
                 DateTime.TryParse(fields[9], out detachmentTime);
                 double.TryParse(fields[10], out tareWeight);
                 double.TryParse(fields[11], out grossWeight);
                 netWeight = grossWeight - tareWeight;
 
+                // TODO:
+                // Clean the data - validate - function.
+                // Check against known issues
+
+                /* Construct the wagon object and add to the list. */
                 wagonDetails data = new wagonDetails(wagonID, origin, plannedDestination, destination, attachmentTime, detachmentTime, netWeight);
                 wagon.Add(data);
 
             }
+            /* Return the completed wagon List. */
+            return wagon;
+        }
 
+        /*
+         * Write the wagon movement details to excel for analysis.
+         */
+        public static void writeToExcel(List<wagonDetails> wagon)
+        {
+            /* May need the PIA for Excel references.
+             * https://msdn.microsoft.com/en-us/library/kh3965hw(v=vs.100).aspx
+             */
+            /* Create the microsfot excel references. */
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel._Workbook workbook;
+            Microsoft.Office.Interop.Excel._Worksheet worksheet;
+
+            /*Start Excel and get Application object. */
+            excel = new Microsoft.Office.Interop.Excel.Application();
+
+            /* Get the reference to the new workbook. */
+            workbook = (Microsoft.Office.Interop.Excel._Workbook)(excel.Workbooks.Add(""));
+            worksheet = (Microsoft.Office.Interop.Excel._Worksheet)workbook.ActiveSheet;
+
+            /* Create the header details. */
+            string[] header = { "Wagon ID", "Origin", "Planned Destiantion", "Destination", "Attatchment Time", "Detatchment Time", "Net Weight" };
+            worksheet.get_Range("A1", "G1").Value2 = header;
+
+            /* Deconstruct the wagon details into excel columns. */
+            string[,] ID = new string[wagon.Count, 1];
+            string[,] Orig = new string[wagon.Count, 1];
+            string[,] Planned = new string[wagon.Count, 1];
+            string[,] Dest = new string[wagon.Count, 1];
+            DateTime[,] attatch = new DateTime[wagon.Count, 1];
+            DateTime[,] detatch = new DateTime[wagon.Count, 1];
+            double[,] weight = new double[wagon.Count, 1];
+
+            /* Loop through the wagon list to deconstruct the data. */
+            for (int i = 0; i < wagon.Count; i++)
+            {
+                ID[i, 0] = wagon[i].wagonID;
+                Orig[i, 0] = wagon[i].origin;
+                Planned[i, 0] = wagon[i].plannedDestination;
+                Dest[i, 0] = wagon[i].destination;
+                attatch[i, 0] = wagon[i].attachmentTime;
+                detatch[i, 0] = wagon[i].detachmentTime;
+                weight[i, 0] = wagon[i].netWeight;
+
+            }
+
+            /* Write the wagon data to the excel file. */
+            worksheet.get_Range("A2", "A" + wagon.Count).Value2 = ID;
+            worksheet.get_Range("B2", "B" + wagon.Count).Value2 = Orig;
+            worksheet.get_Range("C2", "C" + wagon.Count).Value2 = Planned;
+            worksheet.get_Range("D2", "D" + wagon.Count).Value2 = Dest;
+            worksheet.get_Range("E2", "E" + wagon.Count).Value2 = attatch;
+            worksheet.get_Range("F2", "F" + wagon.Count).Value2 = detatch;
+            worksheet.get_Range("G2", "G" + wagon.Count).Value2 = weight;
             
-            /* Write back to excel file. */
+            string savePath = @"C:\Users\Beau\Documents\ARTC\Wagon Volumes";    // home path
+            string saveFilename = savePath + @"\wagonDetails.xlsx";
 
+            /* Check the file does not exist yet. */
+            if (File.Exists(saveFilename))
+                File.Delete(saveFilename);
 
+            /* Save the excel file. */
+            excel.UserControl = false;
+            workbook.SaveAs(saveFilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
-
+            workbook.Close();
+            return;
         }
 
     }
