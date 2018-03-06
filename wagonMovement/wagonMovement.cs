37149494658ec41e385375d6e7572b9d072ef1ff
Beau Bellamy <beau.bellamy@hotmail.com>
2018-03-06 13:49:44 +1100

    Removed unneccessary using directives.

--- wagonMovement/wagonMovement.cs
+++ wagonMovement/wagonMovement.cs
@@ -2,11 +2,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using TrainLibrary;
